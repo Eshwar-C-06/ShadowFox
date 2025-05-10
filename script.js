a714713937cb4e7fcfc22daeeb78e031803503ac
Eshwar-C-06 <eshwarcsr@gmail.com>
2025-05-10 12:33:13 +0530

    img effects and text added 10-5-2025

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-var a=10;
-console.log(a);
+var i=0;
+const txt="Hey, I'm Eshwar — a 19-year-old ECE student from India, mastering circuits by day and Thunder Breathing by night. I craft websites, capture moments, and debug life itself.";
+const speed=40;
+function Typewriter(){
+    if(i<txt.length){
+        document.getElementById("About-typewriter").innerHTML+=txt.charAt(i);
+        i++;
+        setTimeout(Typewriter,speed);
+    }
+}
+window.onload = Typewriter();
